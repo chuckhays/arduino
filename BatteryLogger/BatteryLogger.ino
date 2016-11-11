f128a61b40382094461c1d10bfe5e7a1d51e38d7
Chuck Hays <haysc@google.com>
2016-11-10 17:28:32 -0800

    Copy in battery V display code

--- BatteryLogger/BatteryLogger.ino
+++ BatteryLogger/BatteryLogger.ino
@@ -1,14 +1,117 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <Adafruit_FeatherOLED.h>
-#include <Adafruit_FeatherOLED_WiFi.h>
 
 #define BOARD_LED_PIN             13
 #define VBAT_ENABLED              1
 #define VBAT_PIN                  A7
   
-Adafruit_FeatherOLED_WiFi  oled = Adafruit_FeatherOLED_WiFi();
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+void renderBattery (float battery)
+{
+  #define BATTTEXT_STARTX     77
+  #define BATTTEXT_STARTY     0
+  #define BATTICON_STARTX     110
+  #define BATTICON_STARTY     0
+  #define BATTICON_WIDTH      18
+  #define BATTICON_BARWIDTH3  ((BATTICON_WIDTH - 6) / 3)
+
+
+  // Render the voltage in text
+  display.clearDisplay();
+  display.setCursor(BATTTEXT_STARTX, BATTTEXT_STARTY);
+  display.print(battery, 2);
+  display.println("V");
+  Serial.println("Sending to display");
+
+  // Draw the base of the battery
+  display.drawLine( BATTICON_STARTX + 1,
+            BATTICON_STARTY,
+            BATTICON_STARTX + BATTICON_WIDTH - 4,
+            BATTICON_STARTY,
+            WHITE);
+  display.drawLine( BATTICON_STARTX,
+            BATTICON_STARTY + 1,
+            BATTICON_STARTX,
+            BATTICON_STARTY + 5,
+            WHITE);
+  display.drawLine( BATTICON_STARTX + 1,
+            BATTICON_STARTY + 6,
+            BATTICON_STARTX + BATTICON_WIDTH - 4,
+            BATTICON_STARTY + 6,
+            WHITE);
+  display.drawPixel(BATTICON_STARTX + BATTICON_WIDTH - 3,
+            BATTICON_STARTY + 1,
+            WHITE);
+  display.drawPixel(BATTICON_STARTX + BATTICON_WIDTH - 2,
+            BATTICON_STARTY + 1,
+            WHITE);
+  display.drawLine( BATTICON_STARTX + BATTICON_WIDTH - 1,
+            BATTICON_STARTY + 2,
+            BATTICON_STARTX + BATTICON_WIDTH - 1,
+            BATTICON_STARTY + 4,
+            WHITE);
+  display.drawPixel(BATTICON_STARTX + BATTICON_WIDTH - 2,
+            BATTICON_STARTY + 5,
+            WHITE);
+  display.drawPixel(BATTICON_STARTX + BATTICON_WIDTH - 3,
+            BATTICON_STARTY + 5,
+            WHITE);
+  display.drawPixel(BATTICON_STARTX + BATTICON_WIDTH - 3,
+            BATTICON_STARTY + 6,
+            WHITE);
+
+  // Draw the appropriate number of bars
+  if (battery > 4.26F)
+  {
+    // USB (Solid Rectangle)
+    display.fillRect(BATTICON_STARTX + 2,     // X
+             BATTICON_STARTY + 2,     // Y
+             BATTICON_BARWIDTH3 * 3,  // W
+             3,                       // H
+             WHITE);
+  }
+  else if ((battery <= 4.26F) && (battery >= 4.1F))
+  {
+    // Three bars
+    for (uint8_t i = 0; i < 3; i++)
+    {
+      display.fillRect(BATTICON_STARTX + 2 + (i * BATTICON_BARWIDTH3),
+               BATTICON_STARTY + 2,
+               BATTICON_BARWIDTH3 - 1,
+               3,
+               WHITE);
+    }
+  }
+  else if ((battery < 4.1F) && (battery >= 3.8F))
+  {
+    // Two bars
+    for (uint8_t i = 0; i < 2; i++)
+    {
+      display.fillRect(BATTICON_STARTX + 2 + (i * BATTICON_BARWIDTH3),
+               BATTICON_STARTY + 2,
+               BATTICON_BARWIDTH3 - 1,
+               3,
+               WHITE);
+    }
+  }
+  else if ((battery < 3.8F) && (battery >= 3.4F))
+  {
+    // One bar
+    display.fillRect(BATTICON_STARTX + 2,
+             BATTICON_STARTY + 2,
+             BATTICON_BARWIDTH3 - 1,
+             3,
+             WHITE);
+  }
+  else
+  {
+    // No bars
+  }
+  display.display();
+}
 
 void updateVbat() 
 {
@@ -23,9 +126,10 @@ void updateVbat()
   vbatFloat *= 3.3F;
   vbatFloat /= 1024.0F;
   Serial.println(vbatFloat);
- 
-  oled.setBattery(vbatFloat);
-  oled.refreshIcons();
+
+  renderBattery(vbatFloat);
+  //oled.setBattery(vbatFloat);
+  //oled.refreshIcons();
  
 }
 
@@ -39,18 +143,24 @@ void setup()
   pinMode(BOARD_LED_PIN, OUTPUT);
  
   // Setup the OLED display
-  oled.init();
-  oled.clearDisplay();
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  display.display();
+  delay(1000);
 
-  oled.setBatteryIcon(true);
+  // Clear the buffer.
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.print("Test");
+  display.display();
+  
+  //oled.init();
+  //oled.clearDisplay();
+
+  //oled.setBatteryIcon(true);
   updateVbat();
 }
- 
-/**************************************************************************/
-/*!
-    @brief  This loop function runs over and over again
-*/
-/**************************************************************************/
+
 void loop()
 {
   // Update the battery level
@@ -58,5 +168,5 @@ void loop()
   digitalWrite(BOARD_LED_PIN, HIGH);
   delay(100);
   digitalWrite(BOARD_LED_PIN, LOW);
-  delay(1000);
+  delay(100);
 }
