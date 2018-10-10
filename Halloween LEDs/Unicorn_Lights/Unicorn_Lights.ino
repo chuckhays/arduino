a8dd6722ea5b55e250780857f5e07f7490d7d424
Chuck Hays <github@chuckhays.net>
2018-10-09 20:47:28 -0500

    Add empty methods with docs.

--- Halloween LEDs/Unicorn_Lights/Unicorn_Lights.ino
+++ Halloween LEDs/Unicorn_Lights/Unicorn_Lights.ino
@@ -1,7 +1,4 @@
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
 
 #define PIN 6
 
@@ -15,37 +12,12 @@
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
 
-byte neopix_gamma[] = {
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
-    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
-    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
-    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
-   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
-   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
-   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
-   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
-   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
-   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
-   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
-  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
-  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
-  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
-  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
-
-
 void setup() {
-  pinMode(LEDPIN, OUTPUT);
-  pinMode(SWITCHPIN, INPUT_PULLUP);
-
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-  #if defined (__AVR_ATtiny85__)
-    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-  #endif
-  // End of trinket special code
-  strip.setBrightness(BRIGHTNESS);
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  pinMode(LEDPIN, OUTPUT); // Set switch LED pin to output.
+  pinMode(SWITCHPIN, INPUT_PULLUP); // Set switch pin to input.
+  strip.setBrightness(BRIGHTNESS); // Set max brightness.
+  strip.begin(); // Initialize the neopixel strip object.
+  strip.show(); // Output current LED colors (all off currently).
 }
 
 void loop() {
@@ -62,197 +34,47 @@ void loop() {
     }
   }
   strip.show();
-  /*
-  // Some example procedures showing how to display to the pixels:
-  colorWipe(strip.Color(255, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 255, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 255), 50); // Blue
-  colorWipe(strip.Color(0, 0, 0, 255), 50); // White
-
-  whiteOverRainbow(20,75,5);  
-
-  pulseWhite(5); 
-
-  // fullWhite();
-  // delay(2000);
-
-  rainbowFade2White(3,3,1);
-
-*/
 }
 
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
-}
+// Top
+//   21           43
+// 6  7 20     28 29 42
+// 5  8 19     27 30 41
+// 4  9 18     26 31 40
+// 3 10 17     25 32 39
+// 2 11 16     24 33 38
+// 1 12 15     23 34 37
+// 0 13 14     22 35 36
 
-void pulseWhite(uint8_t wait) {
-  for(int j = 0; j < 256 ; j++){
-      for(uint16_t i=0; i<strip.numPixels(); i++) {
-          strip.setPixelColor(i, strip.Color(0,0,0, neopix_gamma[j] ) );
-        }
-        delay(wait);
-        strip.show();
-      }
+// Tower 0 : Row [0-6] = Index, 13 - Index, Index + 14
+// Tower 1 : Row [0-6] = 22+ Index, 22 + 13 - Index, 22 + Index + 14
+// Tower 0: Row 7 = Index + 14 (or just 21)
+// Tower 0: Row 7 = 22 + Index + 14 (or just 43)
 
-  for(int j = 255; j >= 0 ; j--){
-      for(uint16_t i=0; i<strip.numPixels(); i++) {
-          strip.setPixelColor(i, strip.Color(0,0,0, neopix_gamma[j] ) );
-        }
-        delay(wait);
-        strip.show();
-      }
-}
+// 0::6 means iterate in a loop for each number 0 through 6
+// Tower 0: Column [0-2] = Column * 7 + (0::6)
+// Tower 1: Colun [0-2] = 22 + Column * 7 + (0::6)
 
-
-void rainbowFade2White(uint8_t wait, int rainbowLoops, int whiteLoops) {
-  float fadeMax = 100.0;
-  int fadeVal = 0;
-  uint32_t wheelVal;
-  int redVal, greenVal, blueVal;
-
-  for(int k = 0 ; k < rainbowLoops ; k ++){
-    
-    for(int j=0; j<256; j++) { // 5 cycles of all colors on wheel
-
-      for(int i=0; i< strip.numPixels(); i++) {
-
-        wheelVal = Wheel(((i * 256 / strip.numPixels()) + j) & 255);
-
-        redVal = red(wheelVal) * float(fadeVal/fadeMax);
-        greenVal = green(wheelVal) * float(fadeVal/fadeMax);
-        blueVal = blue(wheelVal) * float(fadeVal/fadeMax);
-
-        strip.setPixelColor( i, strip.Color( redVal, greenVal, blueVal ) );
-
-      }
-
-      //First loop, fade in!
-      if(k == 0 && fadeVal < fadeMax-1) {
-          fadeVal++;
-      }
-
-      //Last loop, fade out!
-      else if(k == rainbowLoops - 1 && j > 255 - fadeMax ){
-          fadeVal--;
-      }
-
-        strip.show();
-        delay(wait);
-    }
-  
-  }
-
-
-
-  delay(500);
-
-
-  for(int k = 0 ; k < whiteLoops ; k ++){
-
-    for(int j = 0; j < 256 ; j++){
-
-        for(uint16_t i=0; i < strip.numPixels(); i++) {
-            strip.setPixelColor(i, strip.Color(0,0,0, neopix_gamma[j] ) );
-          }
-          strip.show();
-        }
-
-        delay(2000);
-    for(int j = 255; j >= 0 ; j--){
-
-        for(uint16_t i=0; i < strip.numPixels(); i++) {
-            strip.setPixelColor(i, strip.Color(0,0,0, neopix_gamma[j] ) );
-          }
-          strip.show();
-        }
-  }
-
-  delay(500);
-
+// Set color for a logical row (0-7), will set all 3 LEDs (or single for top).
+// Tower should be 0 or 1.
+void setRowColor(uint8_t row, uint8_t tower, uint32_t color) {
 
 }
 
-void whiteOverRainbow(uint8_t wait, uint8_t whiteSpeed, uint8_t whiteLength ) {
-  
-  if(whiteLength >= strip.numPixels()) whiteLength = strip.numPixels() - 1;
+// Set color for a logical row(0-7) on both towers at once.
+void setRowColor(uint8_t row, uint32_t color) {
 
-  int head = whiteLength - 1;
-  int tail = 0;
-
-  int loops = 3;
-  int loopNum = 0;
-
-  static unsigned long lastTime = 0;
-
-
-  while(true){
-    for(int j=0; j<256; j++) {
-      for(uint16_t i=0; i<strip.numPixels(); i++) {
-        if((i >= tail && i <= head) || (tail > head && i >= tail) || (tail > head && i <= head) ){
-          strip.setPixelColor(i, strip.Color(0,0,0, 255 ) );
-        }
-        else{
-          strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-        }
-        
-      }
-
-      if(millis() - lastTime > whiteSpeed) {
-        head++;
-        tail++;
-        if(head == strip.numPixels()){
-          loopNum++;
-        }
-        lastTime = millis();
-      }
-
-      if(loopNum == loops) return;
-    
-      head%=strip.numPixels();
-      tail%=strip.numPixels();
-        strip.show();
-        delay(wait);
-    }
-  }
-  
-}
-void fullWhite() {
-  
-    for(uint16_t i=0; i<strip.numPixels(); i++) {
-        strip.setPixelColor(i, strip.Color(0,0,0, 255 ) );
-    }
-      strip.show();
 }
 
+// Set color for a column with column (0-2), will set the entire column.
+// Tower should be 0 or 1.
+void setColumnColor(uint8_t column, uint8_t tower, uint32_t color) {
 
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256 * 5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
 }
 
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
+// Set color for a column with column (0-2) on both towers at once.
+void setColumnColor(uint8_t column, uint32_t color) {
 
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
 }
 
 // Input a value 0 to 255 to get a color value.
@@ -279,4 +101,3 @@ uint8_t green(uint32_t c) {
 uint8_t blue(uint32_t c) {
   return (c);
 }
-
