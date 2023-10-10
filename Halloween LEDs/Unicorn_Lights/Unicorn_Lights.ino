6afc1d0677483cc834cc290d888c40726b71c84b
Chuck Hays <github@chuckhays.net>
2023-10-10 18:17:26 -0500

    Updating for 22 pixel unicorn horn, previous version was the 2 horn version with 2 towers

--- Halloween LEDs/Unicorn_Lights/Unicorn_Lights.ino
+++ Halloween LEDs/Unicorn_Lights/Unicorn_Lights.ino
@@ -2,7 +2,7 @@
 
 #define PIN 6
 
-#define NUM_LEDS 44
+#define NUM_LEDS 22
 
 #define BRIGHTNESS 255
 
@@ -21,78 +21,85 @@ unsigned long last_press_start_time = 0;
 bool switch_pressed = false;
 bool switch_press_recorded = false;
 
-
-void setup() {
-  pinMode(LEDPIN, OUTPUT); // Set switch LED pin to output.
+void setup()
+{
+  pinMode(LEDPIN, OUTPUT);          // Set switch LED pin to output.
   pinMode(SWITCHPIN, INPUT_PULLUP); // Set switch pin to input.
-  strip.setBrightness(BRIGHTNESS); // Set max brightness.
-  strip.begin(); // Initialize the neopixel strip object.
-  strip.show(); // Output current LED colors (all off currently).
+  strip.setBrightness(BRIGHTNESS);  // Set max brightness.
+  strip.begin();                    // Initialize the neopixel strip object.
+  strip.show();                     // Output current LED colors (all off currently).
 
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   // First, check if the switch is pressed.
   checkForSwitchPress();
 
   int sensorValue = analogRead(POTPIN) / 4;
   uint16_t pause = 0;
   // Next, decide which pattern we are currently showing and run it.
-  switch (current_pattern) {
-    case 0:
-      pause = rainbow();
-      break;
-    case 1:
-      pause = alternateRainbow();
-      break;
-    case 2:
-      pause = purple();
-      break;
-    case 3:
-      pause = orange();
-      break;
-    case 4:
-      pause = strobe(sensorValue);
-      break;
-    case 5:
-      pause = strobe(0);
-      break;
-    case 6:
-      pause = fire();
-      break;
-      
+  switch (current_pattern)
+  {
+  case 0:
+    pause = rainbow();
+    break;
+  case 1:
+    pause = alternateRainbow();
+    break;
+  case 2:
+    pause = purple();
+    break;
+  case 3:
+    pause = orange();
+    break;
+  case 4:
+    pause = strobe(sensorValue);
+    break;
+  case 5:
+    pause = strobe(0);
+    break;
+  case 6:
+    pause = fire();
+    break;
   }
   strip.show();
-  
+
   delay(pause);
   analogWrite(LEDPIN, sensorValue);
 }
 
 byte currentRainbowColor = 0;
-uint16_t rainbow() {
-  for(uint16_t i=0; i <= 7; i++) {
-      uint32_t color = Wheel(((i * 36) + currentRainbowColor) & 255);
-      setRowColor(i, color);
+uint16_t rainbow()
+{
+  for (uint16_t i = 0; i <= 7; i++)
+  {
+    uint32_t color = Wheel(((i * 36) + currentRainbowColor) & 255);
+    setRowColor(i, color);
   }
   ++currentRainbowColor;
   return 2;
 }
 
 byte currentAlternateRainbowColor = 0;
-uint16_t alternateRainbow() {
-  for(uint16_t i=0; i <= 2; i++) {
-      uint32_t color = Wheel(((i * 85) + currentAlternateRainbowColor) & 255);
-      setColumnColor(i, color);
-      setRowColor(7, strip.Color(0,0,0,0));
+uint16_t alternateRainbow()
+{
+  for (uint16_t i = 0; i <= 2; i++)
+  {
+    uint32_t color = Wheel(((i * 85) + currentAlternateRainbowColor) & 255);
+    setColumnColor(i, color);
+    setRowColor(7, strip.Color(0, 0, 0, 0));
   }
   ++currentAlternateRainbowColor;
   return 2;
 }
 
 byte currentPurpleRainbowColor = 0;
-uint16_t purple() {
-  for(uint16_t i=0; i <= 7; i++) {
+uint16_t purple()
+{
+  for (uint16_t i = 0; i <= 7; i++)
+  {
     uint32_t color = purpleWheel(((i * 36) + currentPurpleRainbowColor) & 255);
     setRowColor(i, color);
   }
@@ -101,8 +108,10 @@ uint16_t purple() {
 }
 
 byte currentOrange = 0;
-uint16_t orange() {
-  for(uint16_t i=0; i <= 7; i++) {
+uint16_t orange()
+{
+  for (uint16_t i = 0; i <= 7; i++)
+  {
     uint8_t wheelPos = ((i * 36) + currentOrange) & 255;
     uint32_t color = orangeWheel(wheelPos);
     setRowColor(7 - i, color);
@@ -112,13 +121,18 @@ uint16_t orange() {
 }
 
 byte currentStrobe = 0;
-uint16_t strobe(uint16_t sensor) {
+uint16_t strobe(uint16_t sensor)
+{
   uint32_t color = sensor == 0 ? strip.Color(255, 255, 255, 255) : Wheel(sensor & 255);
-  uint32_t off = strip.Color(0,0,0,0);
-  for(uint16_t i=0; i <= 7; i++) {
-    if (currentStrobe == 1) {
+  uint32_t off = strip.Color(0, 0, 0, 0);
+  for (uint16_t i = 0; i <= 7; i++)
+  {
+    if (currentStrobe == 1)
+    {
       setRowColor(i, color);
-    } else {
+    }
+    else
+    {
       setRowColor(i, off);
     }
   }
@@ -126,18 +140,29 @@ uint16_t strobe(uint16_t sensor) {
   return 30 + 30 * currentStrobe;
 }
 
-uint16_t fire() {
+uint16_t fire()
+{
   int r = 255;
   int g = 125;
   int b = 12;
-  for(uint16_t i = 0; i <= 7; ++i) {
-    int flicker = random(0,40);
-    int r1 = r-random(0,40);
-    int g1 = g-random(0,40);
-    int b1 = b-random(0,40);
-    if (r1 < 0) { r1 = 0; }
-    if (b1 < 0) { b1 = 0; }
-    if (g1 < 0) { g1 = 0; }
+  for (uint16_t i = 0; i <= 7; ++i)
+  {
+    int flicker = random(0, 40);
+    int r1 = r - random(0, 40);
+    int g1 = g - random(0, 40);
+    int b1 = b - random(0, 40);
+    if (r1 < 0)
+    {
+      r1 = 0;
+    }
+    if (b1 < 0)
+    {
+      b1 = 0;
+    }
+    if (g1 < 0)
+    {
+      g1 = 0;
+    }
     setRowColor(i, strip.Color(r1, g1, b1));
   }
   return random(50, 150);
@@ -164,70 +189,86 @@ uint16_t fire() {
 
 // Set color for a logical row (0-7), will set all 3 LEDs (or single for top).
 // Tower should be 0 or 1.
-void setRowColor(uint8_t row, uint8_t tower, uint32_t color) {
-  if (row < 7 ) {
+void setRowColor(uint8_t row, uint8_t tower, uint32_t color)
+{
+  if (row < 7)
+  {
     strip.setPixelColor(tower * 22 + row, color);
     strip.setPixelColor(tower * 22 + 13 - row, color);
     strip.setPixelColor(tower * 22 + row + 14, color);
-  } else {
+  }
+  else
+  {
     strip.setPixelColor(tower * 22 + 21, color);
   }
 }
 
 // Set color for a logical row(0-7) on both towers at once.
-void setRowColor(uint8_t row, uint32_t color) {
+void setRowColor(uint8_t row, uint32_t color)
+{
   setRowColor(row, 0, color);
   setRowColor(row, 1, color);
 }
 
-
 // Set color for a column with column (0-2), will set the entire column.
 // Tower should be 0 or 1.
-void setColumnColor(uint8_t column, uint8_t tower, uint32_t color) {
-  for (uint8_t i=0; i<7; i++) {
+void setColumnColor(uint8_t column, uint8_t tower, uint32_t color)
+{
+  for (uint8_t i = 0; i < 7; i++)
+  {
     strip.setPixelColor(tower * 22 + column * 7 + i, color);
   }
 }
 
 // Set color for a column with column (0-2) on both towers at once.
-void setColumnColor(uint8_t column, uint32_t color) {
+void setColumnColor(uint8_t column, uint32_t color)
+{
   setColumnColor(column, 0, color);
   setColumnColor(column, 1, color);
 }
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
+uint32_t Wheel(byte WheelPos)
+{
   WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3,0);
+  if (WheelPos < 85)
+  {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3, 0);
   }
-  if(WheelPos < 170) {
+  if (WheelPos < 170)
+  {
     WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3,0);
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3, 0);
   }
   WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0,0);
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0, 0);
 }
 
-uint32_t orangeWheel(byte WheelPos) {
+uint32_t orangeWheel(byte WheelPos)
+{
   WheelPos = 255 - WheelPos;
-  if(WheelPos < 126) {
-    return strip.Color(255, WheelPos ,0);
+  if (WheelPos < 126)
+  {
+    return strip.Color(255, WheelPos, 0);
   }
   WheelPos -= 126;
-  return strip.Color(255, 129 - WheelPos , 0);
+  return strip.Color(255, 129 - WheelPos, 0);
 }
 
-uint32_t purpleWheel(byte WheelPos) {
-  //WheelPos = 255 - WheelPos;
-  if (WheelPos < 128) {
+uint32_t purpleWheel(byte WheelPos)
+{
+  // WheelPos = 255 - WheelPos;
+  if (WheelPos < 128)
+  {
     byte red = WheelPos <= 125 ? 125 - WheelPos : 0;
     byte green = WheelPos * 2;
     byte blue = 255 - WheelPos * 2;
     return strip.Color(red, green, blue);
-  } else {
-    WheelPos-=128;
+  }
+  else
+  {
+    WheelPos -= 128;
     byte red = WheelPos <= 125 ? WheelPos : 125;
     byte green = 255 - WheelPos * 2;
     byte blue = WheelPos * 2;
@@ -235,38 +276,50 @@ uint32_t purpleWheel(byte WheelPos) {
   }
 }
 
-uint8_t red(uint32_t c) {
+uint8_t red(uint32_t c)
+{
   return (c >> 16);
 }
-uint8_t green(uint32_t c) {
+uint8_t green(uint32_t c)
+{
   return (c >> 8);
 }
-uint8_t blue(uint32_t c) {
+uint8_t blue(uint32_t c)
+{
   return (c);
 }
 
-void checkForSwitchPress() {
+void checkForSwitchPress()
+{
   // Check if the button is pressed.
   uint8_t switchVal = digitalRead(SWITCHPIN);
   // A value of 0 means it is pressed.
-  if (switchVal) {
+  if (switchVal)
+  {
     // Button is not pressed.
     switch_pressed = false;
     switch_press_recorded = false;
-  } else {
-    if (switch_press_recorded) {
+  }
+  else
+  {
+    if (switch_press_recorded)
+    {
       // We already processed this switch press, don't do anything until the button is released.
       return;
     }
     // Button is pressed.
-    if (switch_pressed) {
+    if (switch_pressed)
+    {
       // Button was pressed last time, check if it has been pressed long enough to advance.
-      if (millis() - last_press_start_time > BUTTON_PRESS_DELAY) {
+      if (millis() - last_press_start_time > BUTTON_PRESS_DELAY)
+      {
         // Button was held long enough to qualify as a press.
-        current_pattern = (current_pattern + 1 ) % PATTERNS_COUNT;
+        current_pattern = (current_pattern + 1) % PATTERNS_COUNT;
         switch_press_recorded = true;
       }
-    } else {
+    }
+    else
+    {
       // Button was not pressed last loop, record the time.
       switch_pressed = true;
       last_press_start_time = millis();
